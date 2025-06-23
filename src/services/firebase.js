dee848174dfeef0403d21b198f9e336e3650517f
Daniel Canto <danielgcmenezes2021@gmail.com>
2025-06-23 15:54:00 -0300

    feat: alterando código

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"; // ou getDatabase para Realtime Database
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDEG-URR45zDK4jI3-ASNGTe2YnVMD1ukA",
